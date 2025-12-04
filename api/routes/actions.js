2208640c60d6b0b0fce9dd6f569b80bc4373899a
Muhammad <muhammad124711@gmail.com>
2025-12-05 01:19:17 +0500

    like

--- api/routes/actions.js
+++ api/routes/actions.js
@@ -5,7 +5,7 @@ const { likePost } = require('../controllers/actionController');
 const router = express.Router();
 
 router.post('/posts/:id/like', protect, likePost);
-router.post('/posts/:id/comment', protect, commentPost);
-router.post('/posts/:id/share', protect, sharePost);
+// router.post('/posts/:id/comment', protect, commentPost);
+// router.post('/posts/:id/share', protect, sharePost);
 
 module.exports = router;
