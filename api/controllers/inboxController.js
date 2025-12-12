e6447ad9873559ba606a68648b976149d6b028da
Muhammad <muhammad124711@gmail.com>
2025-12-13 03:02:18 +0500

    search inbox

--- api/controllers/inboxController.js
+++ api/controllers/inboxController.js
@@ -4,12 +4,34 @@ const User = require('../models/User');
 const getUsersByUsername = async (req, res) => {
   try {
     const { username } = req.params;
-    const users = await User.find({ username: { $regex: username, $options: 'i' } }).select('name username avatar').limit(20);
+
+    const exactUserPromises = await User.find({
+      username: { $regex: `^${username}$`, $options: 'i' }
+    }).select('name username avatar').limit(1);
+
+    const exactUser = exactUserPromises.length > 0 ? exactUserPromises[0].toObject() : null;
+
+    if (exactUser) {
+      exactUser.exact = true; 
+    }
+
+    // Find partial matches
+    const excludeFilter = exactUser ? { _id: { $ne: exactUser._id } } : {};
+    const partialUsers = await User.find({
+      username: { $regex: username, $options: 'i' },
+      ...excludeFilter
+    })
+      .select('name username avatar')
+      .limit(exactUser ? 19 : 20)
+      .sort({ username: 1 });
+
+    // Prepend the exact match if it exists
+    const users = exactUser ? [exactUser, ...partialUsers] : partialUsers;
+
     res.status(200).json({ users, success: true });
-  }
-  catch (error) {
+  } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message, success: false });
   }
-}
+};
 
 module.exports = { getUsersByUsername }
